f31ff107d6fb3df60fff325120a847015de140bc
竹田美雪 <takeda.myk@gmail.com>
2016-08-10 14:16:29 +0800

    initial commit

---
+++ tsudukuNikki/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tsudukuNikki
+//
+//  Created by 竹田美雪 on 8/10/16.
+//  Copyright © 2016 竹田美雪. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
