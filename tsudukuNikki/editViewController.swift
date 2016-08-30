9dde107be0c6445fc85f0835c2794810add4c786
竹田美雪 <takeda.myk@gmail.com>
2016-08-30 15:53:16 +0800

    [editViewController] CoreDataのデータ保存可能に(エラー解消) データの読み込み可能に [listViewController] CoreDataから取得した日付を一覧表示させる(未完成)

--- tsudukuNikki/editViewController.swift
+++ tsudukuNikki/editViewController.swift
@@ -47,8 +47,47 @@ class editViewController: UIViewController {
             diaryDatePicker.date = dateFromStr!
             diaryTextView.text  = diaryList[selectedIndex]["diary"]! as String
         }
-
+        read()
+        print(NSBundle.mainBundle())
     }
+    // すでに存在するデータの読み込み処理
+    func read() {
+        let df = NSDateFormatter()
+        df.dateFormat = "yyyy/MM/dd"
+        
+        // AppDeleteをコードで読み込む
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        // Entityの操作を制御するmanagedObjectContextをappDelegateから作成
+        if let managedObjectContext:NSManagedObjectContext = appDelegate.managedObjectContext {
+            
+            // Entityを指定する設定
+            let entityDiscription = NSEntityDescription.entityForName("Diary", inManagedObjectContext: managedObjectContext)
+            
+            let fetchRequest = NSFetchRequest(entityName: "Diary")
+            fetchRequest.entity = entityDiscription
+            
+            // errorが発生した際にキャッチするための変数
+            var error: NSError? = nil
+            
+            // フェッチリクエスト (データの検索と取得処理) の実行
+            do {
+                let results = try managedObjectContext.executeFetchRequest(fetchRequest)
+                print(results.count)
+                for managedObject in results {
+                    let diary = managedObject as! Diary
+                    
+                    var savedDate:NSDate = NSDate(timeIntervalSinceReferenceDate:diary.date)
+                        
+                        print("date: \(df.stringFromDate(savedDate)), detail: \(diary.detail)")
+                }
+            } catch let error1 as NSError {
+                error = error1
+            }
+        }
+    }
+
+    
     //保存ボタンを押したときの処理
     @IBAction func tapSaveBtn(sender: UIButton) {
 //        //日付をString型に変換
@@ -71,21 +110,19 @@ class editViewController: UIViewController {
             let appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate
             
             // Entityの操作を制御するmanagedObjectContextをappDelegateから作成
-            if let managedObjectContext = appDelegate!.managedObjectContext {
+        if let managedObjectContext:NSManagedObjectContext = appDelegate!.managedObjectContext {
                 
                 // 新しくデータを追加するためのEntityを作成します
                 let managedObject: AnyObject = NSEntityDescription.insertNewObjectForEntityForName("Diary", inManagedObjectContext: managedObjectContext)
                 
                 // 「Diary Entity」からObjectを生成し、Attributesに接続して値を代入
                 let diary = managedObject as! Diary
-                diary.date = NSDate()
+                diary.date = diaryDatePicker.date.timeIntervalSinceReferenceDate
                 diary.detail = "日記本文をここに書き込む"
-                //diary.feeling = "Smile"  これで合ってる？
-                
+                diary.feeling = "Smile"  //これで合ってる？
+            
                 // データの保存処理
-                appDelegate.saveContext() // ←←← これ重要
-                
-            }
+                appDelegate!.saveContext() // ←←← これ重要
             
         }else{
 //            diaryList = [["date":dateStr, "diary":diaryTextView.text]]
@@ -100,6 +137,7 @@ class editViewController: UIViewController {
 //        //即反映させる
 //        myDefault.synchronize()
 
+        }
     }
 
     override func didReceiveMemoryWarning() {
--- tsudukuNikki/listViewController.swift
+++ tsudukuNikki/listViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 
 
@@ -18,17 +19,63 @@ class listViewController: UIViewController, UITableViewDataSource, UITableViewDe
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //ユーザーデフォルトを用意する
-        var myDefault = NSUserDefaults.standardUserDefaults()
+//        //ユーザーデフォルトを用意する
+//        var myDefault = NSUserDefaults.standardUserDefaults()
+//        
+//        if (myDefault.objectForKey("diaryList") != nil) {
+//            //データを呼び出して
+//            diaryList = myDefault.objectForKey("diaryList") as! [Dictionary]
+//        }
+//        print(diaryList)
         
-        if (myDefault.objectForKey("diaryList") != nil) {
-            //データを呼び出して
-            diaryList = myDefault.objectForKey("diaryList") as! [Dictionary]
-        }
-        print(diaryList)
-        
-        
+        //CoreData
+        //CoreDataを呼び出す
+        read()
     }
+    //以下でコアデータから取り出した情報を配列に格納する
+    
+    func read() {
+        let df = NSDateFormatter()
+        df.dateFormat = "yyyy/MM/dd"
+        
+        // AppDeleteをコードで読み込む
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        // Entityの操作を制御するmanagedObjectContextをappDelegateから作成
+        if let managedObjectContext:NSManagedObjectContext = appDelegate.managedObjectContext {
+            
+            // Entityを指定する設定
+            let entityDiscription = NSEntityDescription.entityForName("Diary", inManagedObjectContext: managedObjectContext)
+            
+            let fetchRequest = NSFetchRequest(entityName: "Diary")
+            fetchRequest.entity = entityDiscription
+            
+            // errorが発生した際にキャッチするための変数
+            var error: NSError? = nil
+            
+            // フェッチリクエスト (データの検索と取得処理) の実行
+            do {
+                let results = try managedObjectContext.executeFetchRequest(fetchRequest)
+                print(results.count)
+                for managedObject in results {
+                    let diary = managedObject as! Diary
+                    
+                    //配列の作成
+                    //diaryListへの代入
+                    
+                    
+                    
+                    var savedDate:NSDate = NSDate(timeIntervalSinceReferenceDate:diary.date)
+                    
+                    print("date: \(df.stringFromDate(savedDate)), detail: \(diary.detail)")
+                }
+            } catch let error1 as NSError {
+                error = error1
+            }
+        }
+    }
+    //以上でコアデータの読み込みが終了
+
     
         //TableViewに表示する行数を決定する
         func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
